1733c8d987f0e4ef3b99bce1f7aa252ef11abe4c
alber <alber@LAPTOP-74VQQ2EH>
2020-11-11 12:51:41 +0100

    Primera versión da la prueba.

---
+++ src/pruebagit/PruebaGit.java
@@ -0,0 +1,18 @@
+
+package pruebagit;
+
+import java.util.Scanner;
+
+
+public class PruebaGit {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        new Scanner (System.in); 
+        System.out.println("Estoy probando GIT y GIT HUB");    
+        
+    }
+    
+}
